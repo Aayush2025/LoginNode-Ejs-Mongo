333990179fb8e1bf94fd57c920615911b1ce4af8
Aayush Narnoli <112821990+Aayush2025@users.noreply.github.com>
2024-05-02 18:40:05 +0530

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,25 @@
+const mongoose=require("mongoose");
+const connect =mongoose.connect("mongodb://127.0.0.1:27017/Login-tut");
+
+
+connect.then(() => {
+    console.log("Database connected Successfully");
+})
+.catch(()=>{
+    console.log("Database cannot be connected");
+
+});
+
+const LoginSchema =new mongoose.Schema({
+    name: {
+        type:String,
+        required:true
+    },
+    password:{
+    type:String,
+    required:true
+    }
+});
+
+const collection = new mongoose.model("user",LoginSchema);
+module.exports= collection;
